4d13a6b17b0808b07f808be4b0a8df786ece2483
shivamraj27 <vraj12345@yahoo.com>
2026-09-11 14:38:35 +0530

    Layer 1: working data ingestion for prices + SEC filings

--- ingestion/fetch_prices.py
+++ ingestion/fetch_prices.py
@@ -1,4 +1,11 @@
-import yfinance as yf
-df = yf.download("NKE", period="1mo")
-print(df.head())
+# import yfinance as yf
+# df = yf.download("NKE", period="1mo")
+# print(df.head())
 
+
+
+import pandas as pd
+df = pd.read_parquet("data/NKE_financials.parquet")
+print(df[df["tag"] == "Revenues"].sort_values("filed_date").tail(10))
+
+print(df.head())
